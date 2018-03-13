53c3f1263183a12ebb0dff1e0e727f56513af02d
Abe Queen <abedababe8@gmail.com>
2018-03-13 15:29:40 -0700

    functional info at bottom

--- index.js
+++ index.js
@@ -1,163 +1,176 @@
 // document.addEventListener('DOMContentLoaded', function(event){
 const myCart = []
-
+// console.log('hiya')
+const describedItem = []
 const itemData = [
   { name: 'Penne Rigatta',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkDtIE2ZlT-XAnqWLFhhq9Zb7boPQxvHlRR_Wfv3TkjiWdxyQC',
-    description: 'Delicious Penne, veg and meat',
+    desc: 'Whole Wheat Penne pasta with tomatoes and basil',
     type: 'entree',
-    price: '10.50',
-    icon1: true,
+    price: '9.50',
+    icon1: false,
     icon2: true,
     icon3: true,
     icon4: false,
   },
   { name: 'Truffle Pasta',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMXBDjGURLnAiYoZ99_0plsyIKIJd2YCRvPHjVlPauPytoaG5Q1Q',
-    description: 'Pasta with truffles',
+    desc: 'Buttered Pasta with savory organic truffles',
     type: 'entree',
-    price: '15.50',
-    icon1: false,
-    icon2: true,
-    icon3: false,
-    icon4: true,
-  },
-  { name: 'Steak Tips',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyupMobW6mp9DTxSxqC6QDviYUA4pWOcUb9u-m3bDpnYmDiP4D',
-    description: 'good steak',
-    type: 'side',
-    price: '7.50',
-    icon1: true,
-    icon2: false,
-    icon3: false,
-    icon4: true,
-  },
-  { name: 'Chicken with Citrus',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO2wF9wpA2hNSqTr_7OTNnVzf3wOM4tlDjaPpoAeJ9GI7LMSeucA',
-    description: 'slimy chicken boiiiii',
-    type: 'entree',
-    price:'3.00',
-    icon1: true,
-    icon2: false,
-    icon3: false,
-    icon4: true,
-  },
-  { name: 'Chinese noodles',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5ZQ981lVKqrZTAW1R4mjXEOJ5YJ5YwIlpEPbBIYTVr1Kp3pIt1g',
-    description: 'tasty',
-    type: 'side',
-    price: '13.25',
-    icon1: true,
-    icon2: false,
-    icon3: false,
-    icon4: false,
-  },
-  { name:'Chinese dumplings',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx9L-TJT6QHPE3JKQrxMymf52WP6lxW-EkUcWOULo1mn_L0ppCQA',
-    description:'sweet and nutty',
-    type: 'salad',
-    price:'8.00',
+    price: '17.50',
     icon1: false,
     icon2: false,
     icon3: true,
+    icon4: false,
+  },
+  { name: 'Steak Tips',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyupMobW6mp9DTxSxqC6QDviYUA4pWOcUb9u-m3bDpnYmDiP4D',
+    desc: 'Tri-tip quality steak with a house steak sauce',
+    type: 'entree',
+    price: '18.00',
+    icon1: true,
+    icon2: false,
+    icon3: false,
     icon4: true,
   },
-  { name:'shrimp bangaroos',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3w1zibq6O4vt5_B_-PEmk8KdGh_5YmGrQzXc7k5MUvkl4hRO1gA',
-    description:'crunchy, savory, bangaroo',
+  { name: 'Chicken and Veggies',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO2wF9wpA2hNSqTr_7OTNnVzf3wOM4tlDjaPpoAeJ9GI7LMSeucA',
+    desc: 'Carved Rotisseried chicken marinated in a citrusy sauce then topped with onions, bell peppers and cilantro',
     type: 'entree',
     price:'8.50',
     icon1: false,
     icon2: false,
+    icon3: false,
+    icon4: true,
+  },
+  { name: 'Chinese style pan noodles',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5ZQ981lVKqrZTAW1R4mjXEOJ5YJ5YwIlpEPbBIYTVr1Kp3pIt1g',
+    desc: 'Thick rice noodles topped with broccoli and classic mongolian spices',
+    type: 'side',
+    price: '7.00',
+    icon1: true,
+    icon2: true,
     icon3: true,
+    icon4: true,
+  },
+  { name:'Chinese dumplings',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx9L-TJT6QHPE3JKQrxMymf52WP6lxW-EkUcWOULo1mn_L0ppCQA',
+    desc:'Deliciously Doughy Dumplings stuffed with veggies and Pork',
+    type: 'appetizer',
+    price:'6.00',
+    icon1: true,
+    icon2: false,
+    icon3: false,
     icon4: false,
   },
-  { name:'Chocolate Cake',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMVo-OeRcXXk-d2F7ymofbRMHBJzrXx8izKbva15aWR0l9Sf1g7A',
-    description:'death to all',
-    type: 'dessert',
-    price:'5.00',
+  { name:'Shrimp Bangaroos',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3w1zibq6O4vt5_B_-PEmk8KdGh_5YmGrQzXc7k5MUvkl4hRO1gA',
+    desc:'Fried coconut schrimp with house cocktail sauce',
+    type: 'side',
+    price:'7.50',
     icon1: true,
+    icon2: false,
+    icon3: false,
+    icon4: false,
+  },
+  { name:"Devil's Chocolate Cake",
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMVo-OeRcXXk-d2F7ymofbRMHBJzrXx8izKbva15aWR0l9Sf1g7A',
+    desc:"With high cocoa percentage and a fruity, spicy finish, this Devil's cake will have you worshiping it in no time",
+    type: 'dessert',
+    price:'8.00',
+    icon1:true,
     icon2:false,
     icon3:true,
     icon4:false,
   },
-  { name:'Good ol doggys',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAW-IgX4Nhnxkd6Ab2ydxwySw0Xb2PMNsK86HC-q0Kty9SqCkc',
-    description:'nothing left',
-    type: 'appetizer',
-    price:'5.00',
+  { name:'Pomegranate Tofu Salad',
+    img: 'https://www.halfbakedharvest.com/wp-content/uploads/2018/01/Rejuvenating-Winter-Broccoli-Salad-1-500x500.jpg',
+    desc:'An excellent salad topped with market fresh blackberries and grilled tofu',
+    type: 'salad',
+    price:'11.00',
     icon1:false,
     icon2:true,
+    icon3:true,
+    icon4:true,
+  },
+  { name:'Seattle Style Sushi',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxUromC9Zu94XcgzZUQhGlMuScWxAVyUc3F6xgmj7O9FEgdcVjwA',
+    desc:'Tuna and Arbicore wrapped with mango and avocado',
+    type: 'entree',
+    price:'13.00',
+    icon1:false,
+    icon2:false,
     icon3:false,
     icon4:true,
   },
-  { name:'Sushi',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxUromC9Zu94XcgzZUQhGlMuScWxAVyUc3F6xgmj7O9FEgdcVjwA',
-    description:'gonna get a gut disease',
-    type: 'entree',
-    price:'5.00',
-    icon1:true,
-    icon2:true,
-    icon3:false,
-    icon4:false,
-  },
-  { name:'Shrimp and Rice',
+  { name:'Shrimp and brown Rice',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC7LZkkhQJubdBgpa5-nRasf2yGVeXxRl0hqmaoYHxkhmhi5Br',
-    description:'little bois swimmin in my stomach',
-    type: 'dessert',
-    price:'5.00',
-    icon1:false,
+    desc:'Sauteed shrimp on a bed of brown rice with edamame and chili oil',
+    type: 'appetizer',
+    price:'7.50',
+    icon1:true,
     icon2:false,
-    icon3:true,
+    icon3:false,
     icon4:true,
   },
   { name:'Pizzen',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFWX5kL5VeRcZK9XzI7Br0X-mHUDCyT0238m9tQtELIUjDr5Sj',
-    description:'Favorite round food',
+    desc:"Everybody's Favorite round food available with just about every topping",
     type: 'entree',
-    price:'5.00',
+    price:'14.00',
     icon1:true,
     icon2:false,
-    icon3:true,
+    icon3:false,
     icon4:false,
   },
   { name:'Sushi Platter',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyWXP0l8J7d_NcfNl0TvBe3GRF3OOjYE14eyYZ-HELzRIcWgEijA',
-    description:'Aint no party like a salmonela party',
+    desc:"A 'flight' of sushi with California rolls, Philadelphia Rolls, Unagi Rolls and Seattle Style rolls",
     type: 'appetizer',
-    price:'5.00',
-    icon1:false,
+    price:'18.00',
+    icon1:true,
     icon2:false,
-    icon3:true,
+    icon3:false,
     icon4:true,
   },
-  { name:'Avalanche of Tomatos',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2n4kICsIhwozCmxx_L4WJuxF0EOrQJ5CkqjGEn7BhOYSlF04P',
-    description:'good luck',
-    type: 'dessert',
-    price:'5.00',
-    icon1:true,
-    icon2: false,
-    icon3:true,
-    icon4:false,
-  },
-  { name:'Stacked food',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV_lAvZJF2jXxrueZyF7ac5ENnonh_5xg59m9hJ3jZempvNQI5',
-    description:'payin extra for a tower',
-    type: 'appetizer',
-    price:'5.00',
+  { name:'Warm Lentil Salad with Poached Egg',
+    img: 'https://static01.nyt.com/images/2017/05/09/dining/09SALADGUIDE9/09SALADGUIDE9-articleLarge.jpg',
+    desc:'A Paleo diet salad with nuts, lentils and herbs topped with a poached egg',
+    type: 'salad',
+    price:'9.50',
     icon1: false,
-    icon2:true,
-    icon3:true,
-    icon4: false,
+    icon2: true,
+    icon3: true,
+    icon4: true,
+  },
+  { name:'House Special',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV_lAvZJF2jXxrueZyF7ac5ENnonh_5xg59m9hJ3jZempvNQI5',
+    desc:'A tower of deliciousness containing shrimp, kale, flatiron steak and a secret sauce',
+    type: 'entree',
+    price:'13.00',
+    icon1: true,
+    icon2: false,
+    icon3: false,
+    icon4: true,
   },
 ]
 
-
+  const header = document.querySelector('.header')
+  const headerText = document.createElement('p')
+  header.appendChild(headerText)
+  headerText.style.fontFamily = "'Rozha One', serif"
+  headerText.innerHTML = 'Click once to see Item Description and Double click to add to order!'
+  const separate = document.querySelector('.separate')
+  const separateText = document.createElement('p')
+  separate.appendChild(separateText)
+  separateText.style.fontFamily = "'Rozha One', serif"
+  separateText.innerHTML = '<i class="fas fa-fire flame"></i> = Spicy,  <i class="fab fa-vuejs vee"></i> = Vegan,  <i class="fas fa-leaf leaf"></i> = Vegetarian,  <i class="fab fa-pagelines wheat"></i> = Gluten Free'
 
 
   const menuContainer = document.querySelector('.menu-container')
+  const order = document.querySelector('.order')
+  const descMid = document.querySelector('.descMid')
+  const descLeft = document.querySelector('.descLeft')
 //Column 1 setup and interactivity
   const menuCol1 = document.createElement('div')
   menuContainer.appendChild(menuCol1)
@@ -169,7 +182,6 @@ const itemData = [
   const arrow = document.createElement('i')
   arrow.classList.add("fas", "fa-angle-left")
   orderToggle.appendChild(arrow)
-  const order = document.querySelector('.order')
   // toggle order div
   orderToggle.addEventListener('click', function(event){
     const firstChild = event.target.querySelector('.fa-angle-left, .fa-angle-right')
@@ -210,6 +222,16 @@ const itemData = [
     priceEle.innerHTML = price
     return priceEle
   }
+  function createType(type) {
+    const typeEle = document.createElement('p')
+    typeEle.innerHTML = type
+    return typeEle
+  }
+  function createDesc(desc) {
+    const descEle = document.createElement('p')
+    descEle.innerHTML = desc
+    return descEle
+  }
   function addIconCont() {
     const iconCont = document.createElement('div')
     iconCont.style.display = 'flex'
@@ -242,7 +264,9 @@ const itemData = [
   function addImg(imgsrc) {
     const img = document.createElement('img')
     img.setAttribute('src', imgsrc)
-    img.classList.add('img-fluid')
+    // img.classList.add('img-fluid')
+    // img.style.width = '100%'
+    return img
   }
   const orderSection = document.querySelector('.order')
   let subT = 0;
@@ -266,6 +290,11 @@ const itemData = [
     const div = document.createElement('div')
     div.appendChild(createVeil())
     const iconHolder = addIconCont()
+    iconHolder.style.position = 'absolute'
+    iconHolder.style.bottom = '0'
+    iconHolder.style.right = '2.5px'
+    // iconHolder.style.justifyContent = 'right'
+    // iconHolder.style.textAlign = 'right'
     div.classList.add('menuItem')
     div.appendChild(createName(ele.name))
     div.appendChild(createPrice(ele.price))
@@ -286,7 +315,7 @@ const itemData = [
       div.firstChild.classList.add('hide')
     })
     flameButton.addEventListener("click", function(event){
-      if (ele.icon1 !== true ) {
+      if (ele.icon1 !== true) {
         div.firstChild.classList.remove('hide')
       }
     })
@@ -311,7 +340,7 @@ const itemData = [
       }
     })
     appButton.addEventListener('click', function(){
-      if (ele.type !== 'app') {
+      if (ele.type !== 'appetizer') {
         div.firstChild.classList.remove('hide')
       }
     })
@@ -330,9 +359,8 @@ const itemData = [
         div.firstChild.classList.remove('hide')
       }
     })
-    // div.addEventListener('click', function(event){
-    //
-    // })
+
+
     div.addEventListener('dblclick', function(event){
       //add to array
       myCart.push({name:ele.name, price:ele.price})
@@ -341,6 +369,37 @@ const itemData = [
       // rerender orderSection
       orderSection.append(renderCart(myCart))
     })
+    div.addEventListener('click', function(event){
+      describedItem.push({name:ele.name, img: ele.img, desc: ele.desc, type: ele.type, price:ele.price})
+      descMid.innerHTML = ''
+      descMid.style.textAlign = 'center'
+      descMid.style.color = '#e6e6e6'
+      descMid.style.paddingTop = '20px'
+      descMid.append(makeDescribedItem(describedItem))
+
+    })
+    function makeDescribedItem(describedItem){
+      const wideItem = document.createElement('div')
+      wideItem.style.width = '100%'
+      wideItem.appendChild(addImg(ele.img))
+      wideItem.appendChild(createName(ele.name))
+      wideItem.appendChild(createPrice(ele.price))
+      wideItem.appendChild(createType(ele.type))
+      wideItem.appendChild(createDesc(ele.desc))
+      if (ele.icon1 === true) {
+        wideItem.appendChild(addIcon1('<i class="fas fa-fire flame"></i>'))
+      }
+      if (ele.icon2 === true) {
+        wideItem.appendChild(addIcon2('<i class="fab fa-vuejs vee"></i>'))
+      }
+      if (ele.icon3 === true) {
+        wideItem.appendChild(addIcon3('<i class="fas fa-leaf leaf"></i>'))
+      }
+      if (ele.icon4 === true) {
+        wideItem.appendChild(addIcon4('<i class="fab fa-pagelines wheat"></i>'))
+      }
+      return wideItem
+    }
     return div
   })
   flameButton.addEventListener("click", function(event){
@@ -366,7 +425,6 @@ function spawnItems(){
     }
   }
 }
-
 
 function renderCart(myCart){
   const cart = document.createElement('div')
@@ -401,7 +459,6 @@ function renderCart(myCart){
   })
 
   const subTotal = myCart.reduce((acc, ele) => acc + Number(ele.price),0)
-  console.log(subTotal)
   const subRow = document.createElement('div')
     subRow.classList.add('row')
   const subText = document.createElement('div')
